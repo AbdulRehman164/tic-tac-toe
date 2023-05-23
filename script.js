e0b51c61d371105f8112ce31ba6fe8a85dc418ab
Abdul Rehman <abdulrehmang571@gmail.com>
2023-05-23 09:36:29 +0500

    created the Gameboard function and inside it created a 2d array and populated it with 0

---
+++ script.js
@@ -0,0 +1,12 @@
+function Gameboard() {
+  const board = [];
+  for (let i = 0; i < 3; i++) {
+    board[i] = [];
+    for (let j = 0; j < 3; j++) {
+      board[i].push(0);
+    }
+  }
+  return board;
+}
+
+Gameboard();
