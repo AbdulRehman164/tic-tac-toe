37789009f6105034fb8e5dc9775164388e06f281
Abdul Rehman <abdulrehmang571@gmail.com>
2023-05-23 09:40:32 +0500

    created a Cell named function which return two functions getValue and addToken and added the Cell to every element of the board array

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ function Gameboard() {
   for (let i = 0; i < 3; i++) {
     board[i] = [];
     for (let j = 0; j < 3; j++) {
-      board[i].push(0);
+      // eslint-disable-next-line no-use-before-define
+      board[i].push(Cell());
     }
   }
   return board;
+}
+
+function Cell() {
+  let value = 0;
+  const addToken = (player) => {
+    value = player;
+  };
+  const getValue = () => value;
+  return { getValue, addToken };
 }
 
 Gameboard();
